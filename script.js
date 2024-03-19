d483d81f38266252ff538c1ed8d778474e19f9ef
Anna Clara Teixeira De Medeiros <anna.397214@alunosatc.edu.br>
2024-03-19 19:54:11 -0300

    css

--- script.js
+++ script.js
@@ -10,22 +10,46 @@
 // console.log(8**2)
 // console.log(8%2)
 
-console.log("Hello World")
-console.log(typeof "Hello World")
+//console.log("Hello World")
+//console.log(typeof "Hello World")
 
 // concatenacao 
-console.log('qual ' + 'o seu ' + 'nome')
+//console.log('qual ' + 'o seu ' + 'nome')
 
-console.log(`A soma de 5+5 é ${5+5}`)
+//console.log(`A soma de 5+5 é ${5+5}`)
 
-console.log(5==5)
-console.log(5<=5)
-console.log(10>100)
-console.log(10 != 10)
-console.log(!true)
-console.log(!false)
+//console.log(5==5)
+//console.log(5<=5)
+//console.log(10>100)
+//console.log(10 != 10)
+//console.log(!true)
+//console.log(!false)
 
 // declaracao de variaveis
+var nome = 'Anna'
+console.log(nome)
+console.log(typeof nome)
+
+let idade= 18
+console.log(idade)
+console.log(typeof idade)
+
+
+const a=10, b=20, c=30
+console.log(a,b,c)
+
+const user = 'anna'
+if(user == 'anna'){
+    console.log("Olá, Anna")
+}
+
+
+const logado = true
+if(logado) {
+    console.log('Usuário autenticado.')
+} else {
+    console.log('Usuário não autenticado.')
+}
 
 
 
