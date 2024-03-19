5da37471841bed962dbca8dde618e7e982d587da
Anna Clara Teixeira De Medeiros <anna.397214@alunosatc.edu.br>
2024-03-19 20:46:35 -0300

    js

--- script.js
+++ script.js
@@ -26,30 +26,62 @@
 //console.log(!false)
 
 // declaracao de variaveis
-var nome = 'Anna'
-console.log(nome)
-console.log(typeof nome)
+// var nome = 'Anna'
+// console.log(nome)
+// console.log(typeof nome)
 
-let idade= 18
-console.log(idade)
-console.log(typeof idade)
-
-
-const a=10, b=20, c=30
-console.log(a,b,c)
-
-const user = 'anna'
-if(user == 'anna'){
-    console.log("Olá, Anna")
-}
+// let idade= 18
+// console.log(idade)
+// console.log(typeof idade)
 
 
-const logado = true
-if(logado) {
-    console.log('Usuário autenticado.')
-} else {
-    console.log('Usuário não autenticado.')
-}
+// const a=10, b=20, c=30
+// console.log(a,b,c)
+
+// const user = 'anna'
+// if(user == 'anna'){
+//     console.log("Olá, Anna")
+// }
 
 
+// const logado = true
+// if(logado) {
+//     console.log('Usuário autenticado.')
+// } else {
+//     console.log('Usuário não autenticado.')
+// }
+
+
+// //if else de outra forma
+// logado ? console.log("Usuario autenticado.") : console.log('Usuario nao autenticado.')
+
+
+// //switch case, sempre colocar break
+// const curso = 'dafjksn'
+// switch(curso) {
+//     case 'sw':
+//         console.log('Engenharia de Software')
+//         break
+//     case 'ENC':
+//         console.log('Engenharia da computação')
+//         break
+//     case 'engE':
+//         console.log('Engenharia Elétrica')
+//         break
+//     default:
+//         console.log('Nenhuma das alternativas.')
+// }
+
+// //laco de repeticao
+// let i = 0
+// while (i<5) {
+//     console.log(`Iteração ${i+1}`)
+//     i++
+// }
+
+// let j = 10
+// do {
+//     console.log(`Iteração ${j}`)
+//     j -= 1
+// } while (j>0)
 
