8bfd0f18988af831a4645951b05b5f32af313483
Anna Clara Teixeira De Medeiros <anna.397214@alunosatc.edu.br>
2024-03-19 19:41:28 -0300

    js

---
+++ script.js
@@ -0,0 +1,31 @@
+// console.log(typeof 2)
+// console.log(typeof 2.2)
+// console.log(typeof -2)
+
+
+// console.log(2+2)
+// console.log(10-5)
+// console.log(3*2)
+// console.log(4/2)
+// console.log(8**2)
+// console.log(8%2)
+
+console.log("Hello World")
+console.log(typeof "Hello World")
+
+// concatenacao 
+console.log('qual ' + 'o seu ' + 'nome')
+
+console.log(`A soma de 5+5 é ${5+5}`)
+
+console.log(5==5)
+console.log(5<=5)
+console.log(10>100)
+console.log(10 != 10)
+console.log(!true)
+console.log(!false)
+
+// declaracao de variaveis
+
+
+
